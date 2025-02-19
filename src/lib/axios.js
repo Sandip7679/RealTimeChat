a2075717399a6b9ac1ecfe9d79d1fe7760634c92
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-02-19 23:56:33 +0530

    baseUrl changes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: "https://realtimechat-backend-hat4.onrender.com/api",
   withCredentials: true,
 });
