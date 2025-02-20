dc2a6e39a41a719f3066538b62841c4ad60929d3
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-02-20 11:35:30 +0530

    withcredential true

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   baseURL: "https://realtimechat-backend-hat4.onrender.com/api",
-  // withCredentials: true,
+  withCredentials: true,
 });
